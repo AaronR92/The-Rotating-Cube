ea6e1690c50cd2543663c81038c7e9364be3b842
Anton <86726028+AaronR92@users.noreply.github.com>
2022-10-10 17:10:56 +0300

    Icon added

--- src/main/java/com/aaronr92/Main.java
+++ src/main/java/com/aaronr92/Main.java
@@ -1,17 +1,20 @@
 package com.aaronr92;
 
 import javax.swing.*;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         JFrame frame = new JFrame("The Cube");
 
         RotatingCube cube = new RotatingCube();
 
+        frame.setIconImage(new ImageIcon("resources/icon.png").getImage());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.add(cube);
         frame.pack();
+        frame.setResizable(false);
 
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
